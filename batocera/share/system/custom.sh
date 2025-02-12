a90f8f3e68b6d05493a1ee5912103fac2ba47dcc
Adrià Franch <adriadam10@gmail.com>
2025-02-12 19:38:41 +0100

    change source script

--- batocera/share/system/custom.sh
+++ batocera/share/system/custom.sh
@@ -8,5 +8,5 @@ done
 
 if [ $1 == "start" ]
 then 
-	curl -s -L https://raw.githubusercontent.com/WizzardSK/gameflix/main/batocera.sh | bash
+	curl -s -L https://raw.githubusercontent.com/adriadam10/gameflix/main/batocera.sh | bash
 fi
