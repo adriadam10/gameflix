097d6ff8b6c4e3a721c0c682364a5eebdc722acf
Adrià Franch <adriadam10@gmail.com>
2025-02-19 19:55:34 +0100

    log batocera script execution

--- batocera/share/system/custom.sh
+++ batocera/share/system/custom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ "$1" == "start" ]
 then 
-	curl -s -L https://raw.githubusercontent.com/adriadam10/gameflix/main/batocera.sh | bash
+	curl -s -L https://raw.githubusercontent.com/adriadam10/gameflix/main/batocera.sh | bash &> script.log
 fi
